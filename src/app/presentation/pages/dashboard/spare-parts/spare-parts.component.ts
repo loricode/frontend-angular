ab116c106651ea0a2181da3d81f42e449ade7643
loricode <loritec26@gmail.com>
2024-11-19 21:57:10 -0500

    currency

--- src/app/presentation/pages/dashboard/spare-parts/spare-parts.component.ts
+++ src/app/presentation/pages/dashboard/spare-parts/spare-parts.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { NgIf } from '@angular/common';
+import { CurrencyPipe, NgIf } from '@angular/common';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HeaderOptionComponent } from '../../../components/header-option/header-option.component';
 import { ToastrService } from 'ngx-toastr';
@@ -8,7 +8,7 @@ import { SparePartService } from '../../../../data/spare-part/spare-part.service
 @Component({
   selector: 'app-spare-parts',
   standalone: true,
-  imports: [HeaderOptionComponent, ReactiveFormsModule, NgIf],
+  imports: [HeaderOptionComponent, ReactiveFormsModule, CurrencyPipe, NgIf],
   templateUrl: './spare-parts.component.html',
   styleUrl: './spare-parts.component.css'
 })
@@ -16,7 +16,7 @@ export class SparePartsComponent {
   private toastr = inject(ToastrService);
   private fb = inject(FormBuilder);
   private sparePartService = inject(SparePartService);
-  arrayData: any[] = [{code:'jkjk', price:42334, quantity:4, reference:'jjjj', dsc:'lloo'}];
+  arrayData: any[] = [{code:'1002813', price:35000, quantity:4, reference:'jjjj', dsc:'gas refrigerante R134A'}];
 
   form = this.fb.group({
     sparePartField: ['', [Validators.required, Validators.maxLength(250)]],
